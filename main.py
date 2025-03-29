35b65a09487d2ef87e6bf1d9a6b724b5d1d96a2b
Jordan <jordanmarkus7444@gmail.com>
2025-03-29 03:33:00 -0500

    I think i finished the CalculateSkins.py file, and called it correctly in the main.py, it returns a dictionary where the key is the golfer_id and the value is the amount of skins they won. Right now this function doesn't return the money each player made, but we could probably do that in the javascript since all we need to do is loop over each players skins won, and divide that by the total skins and multiply that to the total money

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from pythonFiles import *
+from typing import Dict, List
+from pythonFiles import calculate_stableford, update_handicap, calculate_skins
 
 
 app = FastAPI()
 
+class Skins(BaseModel):
+    scores: Dict[int, List[int]] # key is golfer id, and the list[int] is a list of scores for the golfer
+    money: int
+    course_id: int
+
+@app.post("/skins")
+def get_skins(round_data: Skins):
+    points = calculate_skins(round_data.scores, round_data.course_id, round_data.money)
+    return {"skins_points": points} # points should be a dictionary key being golfer_id and value being amount of skins won
+
+
+
 class Stableford(BaseModel):
     golfer_id: int    # Player's id
-    scores: list[int] # Player's scores per hole
+    scores: List[int] # Player's scores per hole
     course_id: int    # course id to get pars per hole
 
 @app.post("/stableford")
-def get_stableford_points(round_data: Stableford):
-    points = calculate_stableford(round_data.scores, round_data.course_id)
-    
+def get_stableford_points(round_data: Stableford): # takes in golfer_id, scores, course_id
+    points = calculate_stableford(round_data.scores, round_data.course_id) # calculates stableford points and returns it
+    update_handicap(round_data.golfer_id, points) # updates golfers handicap
     return {"stableford_points": points}
 
