9b1fe76532716a0c3ab441f301a78f1e51b1cd87
Jordan <jordanmarkus7444@gmail.com>
2025-03-29 03:36:45 -0500

    quick fix

--- pythonFiles/CalculateSkins.py
+++ pythonFiles/CalculateSkins.py
@@ -4,7 +4,6 @@ def calculate_skins(self, scores, course_id, money = 0): # scores dict key=int v
     current_winning_id = 0
     current_lowest_score = 99
     is_a_tie = False
-    counter = 0
     total_skins = 0
     skins_won = {} # key is golfer_id and value is list of skins won -- 1 -> ["2,3,4"]
     points = {} #points should be a dictionary, key being golfer_id and value being amount of skins won -- 1 -> 3
@@ -37,7 +36,14 @@ def calculate_skins(self, scores, course_id, money = 0): # scores dict key=int v
         current_lowest_score = 99
         current_winning_id = 0
 
+    insert_data_into_db(self, skins_won, total_skins, money, scores, course_id)
 
+    return points
+
+
+
+# This function inputs all the data we gathered into the database
+def insert_data_into_db(self, skins_won, total_skins, money, scores, course_id):
     scores_id = [] # this is for the games table, we need to put all the ids of the skins_score's inserted below into this
     month = self.calander.selectedDate().month()
     day = self.calander.selectedDate().day()
@@ -54,12 +60,34 @@ def calculate_skins(self, scores, course_id, money = 0): # scores dict key=int v
         money_made = round((skins_amount / total_skins) * money, 2)
         hole_scores = ", ".join(map(str, scores[golfer_id]))
 
-
-        self.cursor.execute("INSERT into skins_score (golfer_id, money, course_id, week, hole_scores, skins_points, holes_won) VALUES (?,?,?,?,?,?,?)"(golfer_id, money_made, course_id, week, hole_scores, skins_amount, skins))
+        self.cursor.execute("INSERT into skins_score (golfer_id, money, course_id, week, hole_scores, skins_points, holes_won) VALUES (?,?,?,?,?,?,?)", (golfer_id, money_made, course_id, week, hole_scores, skins_amount, skins))
         self.conn.commit()
-        scores_id.append(self.cursor.lastrowid)
+
+        scores_id.append(self.cursor.lastrowid) 
+    
+    score_ids_str = ",".join(str(id) for id in scores_id) # this makes the list of ids into a string of ids
+    self.cursor.execute("INSERT INTO games(game_type, scores_ids) VALUES (?,?)", (1, score_ids_str,))
+    self.conn.commit()
+
+    game_id = self.cursor.lastrowid
+    game_num = 1
+    session_id = 0
+    
+    self.cursor.execute("SELECT * FROM sessions WHERE day = ?", (week,))
+    results = self.cursor.fetchall()
+    count = len(results)
+
+    if (count > 0):
+        session_id = results[0][1]
+        game_num = count + 1
+    else:
+        self.cursor.execute("SELECT MAX(session_id) FROM sessions")
+        result = self.cursor.fetchone()[0]  # This gets the max session_id
+        session_id = (result if result is not None else 0) + 1  # Default to 1 if no entries exist
+        
+    # Insert data into sessions table
+    self.cursor.execute("INSERT INTO sessions(session_id, game_num, day, game_type, game_id) VALUES (?,?,?,?,?)", (session_id, game_num, week, 1, game_id))
+    self.conn.commit()
+
     
 
-
-
-    return points
